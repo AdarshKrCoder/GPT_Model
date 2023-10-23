37d7f2ac5669a62345accf99c765fb027a0b326c
AdarshKrCoder <adarshkumarbharat21@gmail.com>
2023-10-23 12:35:57 +0530

    created folders

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog}from './blogs/blog'
+export {default as Features} from './feature/Features';
+export {default as Footer} from './footer/Footer';
+export {default as Header} from './header/Header';
+export {default as Possibility}from './possibility/Possibility'
+export {default as whatGPT3} from './whatGPT3/whatGPT3';
---
+++ src/containers/possibility/Possibility.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './possibility.css'
+
+const Possibility = () => {
+  return (
+    <div>Possiblity</div>
+  )
+}
+
+export default Possibility
